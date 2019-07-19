769fb55378be4da8599c233317d091dc9b0eac01
Roman Suldin <suldinroman@yandex.ru>
2019-07-19 13:41:20 +0300

    Parallelelism was deleted

--- source.cpp
+++ source.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <omp.h>
-
 
 int main()
 {
-#pragma omp paraller
-    {
-    	std::cout << "Hello world!" << std::endl;
-    }
-    return 0;
+	std::cout << "Hello world!" << std::endl;
+	return 0;
 }
